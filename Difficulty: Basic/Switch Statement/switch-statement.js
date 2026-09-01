10e4f66f3fdc038dc73a5cb0a7d72b3cfd70eb5c
Naveen Kambhampati <kambhampatinaveen5@gmail.com>
2026-09-02 01:02:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Switch Statement/switch-statement.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const n = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number)[0];
+
+// code here
+switch (n){
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    case 9:
+        console.log("Nine");
+        break;
+    default:
+        console.log("Unknown")
+        break;
+}
