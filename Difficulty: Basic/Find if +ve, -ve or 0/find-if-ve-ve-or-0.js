0c49425c330d8908b969e1c7aa7479d7b9be8d12
Naveen Kambhampati <kambhampatinaveen5@gmail.com>
2026-09-04 23:53:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find if +ve, -ve or 0/find-if-ve-ve-or-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    utility(a) {
+        // code here
+        if(a > 0){
+            console.log("Positive")
+        }
+        else if(a < 0){
+            console.log("Negative")
+        }
+        else{
+            console.log("Zero")
+        }
+    }
+}
